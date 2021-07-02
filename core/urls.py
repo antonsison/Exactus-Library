6f8c622dd86bde8a3ab7b5add65273c0f5300307
Anton Sison <antoninakisison@gmail.com>
2021-07-02 11:04:21 +0800

    update urls and add base template

--- core/urls.py
+++ core/urls.py
@@ -13,9 +13,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
+
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
+
+from django.views.generic.base import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    re_path('(.*)', TemplateView.as_view(template_name='base.html'), name="base"),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
