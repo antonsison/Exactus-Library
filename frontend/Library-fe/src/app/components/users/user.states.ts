a5793a821f42fbfa796d650b00d33aba3ea3df5d
Anton Sison <antoninakisison@gmail.com>
2021-07-04 01:47:21 +0800

    created components for user app

---
+++ frontend/Library-fe/src/app/components/users/user.states.ts
@@ -0,0 +1,31 @@
+import { ContentOnly, NavContent } from "src/app/commons/utils/layout.utils";
+import { LoginComponent } from "./login/login.component";
+import { Disconnect, LoginRequired } from "src/app/commons/utils/security.utils";
+import { RegisterComponent } from "./register/register.component";
+import { SettingsComponent } from "./settings/settings.component"
+
+export const USER_STATES: Object[] = [
+    {
+        name: 'login',
+        url : '/login/',
+        views: ContentOnly(LoginComponent),
+        params: { next: window.location.pathname }
+    },
+    {
+        name    : 'logout',
+        url     : '/logout/',
+        onEnter : Disconnect
+    },
+    {
+        name    : 'register',
+        url     : '/register/',
+        views: ContentOnly(RegisterComponent),
+        params: { next: window.location.pathname }
+    },
+    {
+        name : 'settings',
+        url  : '/settings/',
+        views:  NavContent(SettingsComponent),
+        onEnter: LoginRequired
+    },
+];
