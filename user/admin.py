e309b59b08592b1bad8a9b86de85eccb179c2111
Anton Sison <antoninakisison@gmail.com>
2021-07-04 18:47:58 +0800

    add user model to admin

--- user/admin.py
+++ user/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import User
 
 # Register your models here.
+class UserAdmin(admin.ModelAdmin):
+
+    model = User
+    readonly_fields = ('date_joined',)
+    ordering = ('date_joined',)
+    list_display = ('email', 'firstname', 'lastname', 'about_me', 'date_joined', 'is_active', 'is_staff', 'is_superuser', 'avatar')
+
+
+admin.site.register(User, UserAdmin)
