7778c9c2e74c0f1df359caff7859b8d53b992e43
Anton Sison <antoninakisison@gmail.com>
2021-07-04 21:23:07 +0800

    create users api

--- user/api.py
+++ user/api.py
@@ -5,14 +5,19 @@ from django.conf import settings
 from django.http import HttpResponseRedirect
 
 from rest_framework.authtoken.models import Token
+from rest_framework.parsers import MultiPartParser
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.response import Response
+from rest_framework.viewsets import ViewSet
 from rest_framework.views import APIView
 
 from .serializers import (
     AuthTokenSerializer,
     RegisterSerializer,
+    UserSerializer,
 )
+
+from .models import User
 
 class Login(APIView):
 
@@ -54,3 +59,44 @@ class Register(APIView):
             token, created = Token.objects.get_or_create(user=user)
             return Response(status=200)
         return Response(serializer.errors, status=400)
+
+
+class User(ViewSet):
+
+    serializer_class = UserSerializer
+    permission_classes = (AllowAny,)
+    parser_class = (MultiPartParser,)
+
+    def get(self, *args, **kwargs):
+        serializer = self.serializer_class(
+            instance=self.request.user
+        )
+        return Response(serializer.data, status=200)
+
+    def get_current(self, *args, **kwargs):
+        user = User.objects.get(id=self.request.GET.get('id'))
+        serializer = self.serializer_class(
+            instance=user
+        )
+        return Response(serializer.data, status=200)
+
+    def update(self, *args, **kwargs):
+        serializer = self.serializer_class(
+            data=self.request.data,
+            instance=self.request.user
+        )
+
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=200)
+
+
+class Users(ViewSet):
+
+    serializer_class = UserSerializer
+    permission_classes = (AllowAny,)
+
+    def get(self, *args, **kwargs):
+        users = User.objects.all()
+        serializer = self.serializer_class(users, many=True)
+        return Response(serializer.data, status=200)
