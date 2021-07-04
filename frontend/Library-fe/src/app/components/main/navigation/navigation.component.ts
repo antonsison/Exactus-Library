a284d80461e2ad1e5fcd95d234102501c9388f7d
Anton Sison <antoninakisison@gmail.com>
2021-07-04 16:34:03 +0800

    updated footer and navigation

--- frontend/Library-fe/src/app/components/main/navigation/navigation.component.ts
+++ frontend/Library-fe/src/app/components/main/navigation/navigation.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { StateService } from '@uirouter/core';
+import { AuthService } from 'src/app/commons/services/auth/auth.service';
+import { User } from 'src/app/commons/models/user.model';
 
 @Component({
   selector: 'app-navigation',
@@ -7,9 +10,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavigationComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    public state: StateService,
+    public auth: AuthService,
+  ) { }
 
-  ngOnInit(): void {
+  async ngOnInit() {
+
+    // if (this.auth.user.id == null) {
+    //   await this.auth.setuser();
+    // }
+
+    // this.auth.getUser();
+
+  }
+
+  dropdownClick(event){
+    event.preventDefault();
+  }
+
+  navigationRedirect(event, route){
+    event.preventDefault();
+    this.state.go(route);
   }
 
 }
