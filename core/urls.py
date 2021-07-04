ecfef8d40664d29c72719ed755778af1138810ac
Anton Sison <antoninakisison@gmail.com>
2021-07-04 18:49:16 +0800

    add user urls to core urls

--- core/urls.py
+++ core/urls.py
@@ -17,11 +17,12 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 from django.contrib import admin
-from django.urls import path, re_path
+from django.urls import path, re_path, include
 
 from django.views.generic.base import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/user/', include('user.urls')),
     re_path('(.*)', TemplateView.as_view(template_name='base.html'), name="base"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
