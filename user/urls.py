397b761ede3883b7fa6e92d6e230b727656fc5aa
Anton Sison <antoninakisison@gmail.com>
2021-07-04 21:23:42 +0800

    update user urls

--- user/urls.py
+++ user/urls.py
@@ -1,7 +1,17 @@
 from django.urls import path, re_path
-from .api import Login, Logout, Register
+from .api import Login, Logout, Register, User, Users
 
 urlpatterns = [
+    path('', Users.as_view({
+        'get': 'get',
+    }), name="users"),
+    path('auth/', User.as_view({
+        'get': 'get',
+        'post': 'update',
+    }), name="user_detail"),
+    path('auth-user/', User.as_view({
+        'get': 'get_current',
+    }), name="current-user"),
     path('login/', Login.as_view(), name="login"),
     path('logout/', Logout.as_view(), name="logout"),
     path('register/', Register.as_view(), name="register"),
