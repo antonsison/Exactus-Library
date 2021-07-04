9f7dfac8b03869e8400aec619e22157e397e936c
Anton Sison <antoninakisison@gmail.com>
2021-07-04 20:31:37 +0800

    updated user model to accomodate created fields

--- frontend/Library-fe/src/app/commons/models/user.model.ts
+++ frontend/Library-fe/src/app/commons/models/user.model.ts
@@ -1,17 +1,13 @@
 export class User {
     id            : string = null;
     email         : string = null;
-    username      : string = null;
     first_name    : string = null;
     last_name     : string = null;
     full_name     : string = null;
     has_usable_pass : boolean = null;
-    icon         : any = null;
+    avatar         : any = null;
     date_joined  : string = null;
     date_updated : string = null;
-    permission   : string = null;
-    live_count   : number = null;
-    backtest_count: number = null;
 
     constructor(data={}) {
       Object.assign(this, data);
@@ -23,8 +19,8 @@ export class ShortUser {
     id          : string = null;
     email       : string = null; 
     username    : string = null;
-    first_name  : string = null;
+    firstname  : string = null;
     full_name   : string = null;
-    last_name   : string = null;
+    lastname   : string = null;
     avatar       : string = null;
 }
